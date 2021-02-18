4c5184c45ffd9f83f7d97aa33e08b5a69cfd98ca
Ítallo <italloeco@gmail.com>
2021-02-18 14:08:19 -0300

    Configuração Identity, Repositórios e Cookies

---
+++ GerenciadorDeCondominios.DAL/ConfiguracaoRepositoriosExtension.cs
@@ -0,0 +1,17 @@
+﻿using GerenciadorDeCondominios.DAL.Intefaces;
+using GerenciadorDeCondominios.DAL.Repositorios;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciadorDeCondominios.DAL
+{
+    public static class ConfiguracaoRepositoriosExtension
+    {
+        public static void ConfigurarRepositorios(this IServiceCollection services)
+        {
+            services.AddTransient<IUsuarioRepositorio, UsuarioRepositorio>();
+        }
+    }
+}
